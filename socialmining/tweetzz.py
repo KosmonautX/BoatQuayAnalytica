bdf9ac77c6253d385eaa0ca36b0bb08c0d5fe57f
Vivekbala <avivekbala@gmail.com>
2018-03-15 18:11:56 +0800

    twitter

--- socialmining/tweetzz.py
+++ socialmining/tweetzz.py
@@ -1,16 +1,16 @@
 import tweepy
 from textblob import TextBlob
-key = '	baui93JzNSGkKQytDsYivLSlb'
+key = 'baui93JzNSGkKQytDsYivLSlb'
 secret = '66cy90H5v3iCWVPYSQkELIAVS1SU8ogNuLmuMRYkk1R7G307vs'
 
 access_token = '3284166475-tIwgFGIbVjVHMcZxHrJsIlFImWLVtOBD2iTIfkY'
 access_secret = 'B5wDTc7ZO6lKzIUKJ5yQmeOd5Jn86b1m1oOKjKf3Jijlb'
-auth = tweepy.OAuthHandler(key,secret)
-auth.set_access_token(access_token,access_secret)
+auth = tweepy.OAuthHandler(key, secret)
+auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
-public_tweets = api.search('gst')
+public_tweets = api.search('lee kuan yew')
 
-for tweet in public_tweets
+for tweet in public_tweets:
     print(tweet.text)
     analysis = TextBlob(tweet.text)
     print(analysis.sentiment)
